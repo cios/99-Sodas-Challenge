0e552e9e82e22aa06eb11e7e08974aea88d58c61
Daniel Esquivias <danielesquivias@daniels-mbp.hsd1.ca.comcast.net>
2014-09-16 13:36:57 -0700

    Initial Commit

---
+++ 99SodasChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  99SodasChallenge
+//
+//  Created by Daniel Esquivias on 9/16/14.
+//  Copyright (c) 2014 Complete iOS 8 with Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ 99SodasChallengeTests/_9SodasChallengeTests.swift
@@ -0,0 +1,36 @@
+//
+//  _9SodasChallengeTests.swift
+//  99SodasChallengeTests
+//
+//  Created by Daniel Esquivias on 9/16/14.
+//  Copyright (c) 2014 Complete iOS 8 with Swift. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class _9SodasChallengeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
