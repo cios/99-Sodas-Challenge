11b2410bef2d33863f7d07bcfd196078e6fc43b6
Daniel Esquivias <danielesquivias@daniels-mbp.hsd1.ca.comcast.net>
2014-09-16 13:47:24 -0700

    Challenge code complete

--- 99SodasChallenge/ViewController.swift
+++ 99SodasChallenge/ViewController.swift
@@ -13,6 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        for var sodas = 99; sodas >= 0; sodas-=1{
+            if sodas > 0{
+                println("\(sodas) bottles of soda on the wall")
+            }else{
+                println("No more bottles of soda on the wall")
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
